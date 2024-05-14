b2f919e550473cade6d229acd7abeaabb013456d
zalviandyr <zukronalviandy@gmail.com>
2024-05-14 08:55:05 +0700

    feat: update module

--- src/main/kotlin/com/alurkerja/alurin/AlurkerjaKotlinApplication.kt
+++ src/main/kotlin/com/alurkerja/alurin/AlurkerjaKotlinApplication.kt
@@ -1,4 +1,4 @@
-package com.alurkerja.alurin.alurkerjakotlin
+package com.alurkerja.alurin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
